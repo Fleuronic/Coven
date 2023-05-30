0eec8067f14992edd2053624d44a1725e7dbd8c9
Jordan Kay <jkay@collegeboard.org>
2023-05-30 12:46:26 -0400

    WIP

--- Modules/Features/Tests/DemoListTests/DemoListWorkflowTests.swift
+++ Modules/Features/Tests/DemoListTests/DemoListWorkflowTests.swift
@@ -6,6 +6,7 @@ import EnumKit
 
 import enum Demo.Demo
 
+@testable import Ergo
 @testable import WorkflowContainers
 @testable import WorkflowReactiveSwift
 @testable import enum DemoList.DemoList
@@ -21,6 +22,36 @@ final class DemoListWorkflowTests: XCTestCase {
 			.assert(output: demo)
 	}
 
+    func testShowDemos() {
+        let demos = Demo.allCases
+        let workflow = DemoList.Workflow()
+        
+        DemoList.Workflow.Action
+            .tester(withState: workflow.makeInitialState())
+            .send(action: .show(demos))
+            .verifyState { XCTAssertEqual($0.demos, demos) }
+            .assertNoOutput()
+        
+        DemoList.Workflow.Action
+            .tester(
+                withState: .init(
+                    demos: demos,
+                    updateWorker: .ready(to: workflow.updateDemos)
+                )
+            )
+            .send(action: .show(nil))
+            .verifyState { XCTAssertEqual($0.demos, demos) }
+            .assertNoOutput()
+    }
+    
+    func testUpdateDemos() {
+        DemoList.Workflow.Action
+            .tester(withState: DemoList.Workflow().makeInitialState())
+            .send(action: .updateDemos)
+            .verifyState { XCTAssert($0.updateWorker.isWorking) }
+            .assertNoOutput()
+    }
+
 	func testRenderingScreen() throws {
 		let demos = Demo.allCases
 
@@ -34,58 +65,52 @@ final class DemoListWorkflowTests: XCTestCase {
 				let alertScreen = try XCTUnwrap(item.screen.wrappedScreen as? Alert.Screen<DemoList.Screen>)
 				let screen = alertScreen.baseScreen
 				XCTAssertEqual(screen.demos, demos)
+                
+                let barContent = try XCTUnwrap(item.barVisibility[expecting: Bar.Content.self])
+                XCTAssertEqual(barContent.title, "Workflow Demo")
 			}
+            .verifyState { state in
+                XCTAssertEqual(state.demos, demos)
+                XCTAssertTrue(state.updateWorker.isReady)
+            }
 	}
 
-	func testRenderingBarContent() throws {
-		try DemoList.Workflow()
-			.renderTester()
-			.expectWorkflow(
-				type: WorkerWorkflow<DemoList.Workflow.UpdateWorker>.self,
-				producingRendering: ()
-			)
-			.render { item in
-				let barContent = try XCTUnwrap(item.barVisibility[expecting: Bar.Content.self])
-				XCTAssertEqual(barContent.title, "Workflow Demo")
-			}
-	}
-
-	func testRenderingUpdateDemos() throws {
-		let workflow = DemoList.Workflow()
-
-		try workflow
-			.renderTester()
-			.expectWorkflow(
-				type: WorkerWorkflow<DemoList.Workflow.UpdateWorker>.self,
-				producingRendering: ()
-			)
-			.render { item in
-				let barContent = try XCTUnwrap(item.barVisibility[expecting: Bar.Content.self])
-				let rightItem = try XCTUnwrap(barContent.rightItem)
-//				XCTAssertEqual(rightItem.content, .text("Update"))
-			}
-			.assert(action: DemoList.Workflow.Action.updateDemos)
-			.assertNoOutput()
-
-//		try workflow
-//			.renderTester(
-//				initialState: .init(
-//					demos: Demo.allCases,
-//					updateWorker: .working(to: workflow.updateDemos)
-//				)
-//			)
-//			.expectWorkflow(
-//				type: WorkerWorkflow<DemoList.Workflow.UpdateWorker>.self,
-//				producingRendering: ()
-//			)
-//			.render { item in
-//				let barContent = try XCTUnwrap(item.barVisibility[expecting: Bar.Content.self])
-//				let rightItem = try XCTUnwrap(barContent.rightItem)
-//				XCTAssertFalse(rightItem.isEnabled)
-//			}
-//			.assert(action: DemoList.Workflow.Action.updateDemos)
-//			.assertNoOutput()
-	}
+    func testRenderingUpdateDemos() throws {
+        let workflow = DemoList.Workflow()
+        
+        try workflow
+            .renderTester()
+            .expectWorkflow(
+                type: WorkerWorkflow<DemoList.Workflow.UpdateWorker>.self,
+                producingRendering: ()
+            )
+            .render { item in
+                let barContent = try XCTUnwrap(item.barVisibility[expecting: Bar.Content.self])
+                let rightItem = try XCTUnwrap(barContent.rightItem)
+                XCTAssertEqual(rightItem.content, .text("Update"))
+                rightItem.handler()
+            }
+            .assert(action: DemoList.Workflow.Action.updateDemos)
+            .assertNoOutput()
+        
+        try workflow
+            .renderTester(
+                initialState: .init(
+                    demos: Demo.allCases,
+                    updateWorker: .working(to: workflow.updateDemos)
+                )
+            )
+            .expectWorkflow(
+                type: WorkerWorkflow<DemoList.Workflow.UpdateWorker>.self,
+                producingRendering: ()
+            )
+            .render { item in
+                let barContent = try XCTUnwrap(item.barVisibility[expecting: Bar.Content.self])
+                let rightItem = try XCTUnwrap(barContent.rightItem)
+                XCTAssertFalse(rightItem.isEnabled)
+            }
+            .assertNoOutput()
+    }
 
 	func testRenderingSelectDemo() throws {
 		let demo = Demo.swiftUI
@@ -105,6 +130,77 @@ final class DemoListWorkflowTests: XCTestCase {
 			.assert(action: DemoList.Workflow.Action.demo(demo))
 			.assert(output: demo)
 	}
+    
+    func testRenderingAlert() throws {
+        let workflow = DemoList.Workflow()
+
+        try DemoList.Workflow()
+            .renderTester(
+                initialState: .init(
+                    demos: Demo.allCases,
+                    updateWorker: .init(
+                        state: .failed(.loadError),
+                        return: workflow.updateDemos
+                    )
+                )
+            )
+            .expectWorkflow(
+                type: WorkerWorkflow<DemoList.Workflow.UpdateWorker>.self,
+                producingRendering: ()
+            )
+            .render { backStackScreen in
+                let wrappedScreen = backStackScreen.screen.wrappedScreen
+                let alertScreen = try XCTUnwrap(wrappedScreen as? Alert.Screen<DemoList.Screen>)
+                let alert = try XCTUnwrap(alertScreen.alert)
+                XCTAssertEqual(alert.title, "Update Error")
+                XCTAssertEqual(alert.message, "The demos could not be updated. Please try again later.")
+                
+                let dismissAction = try XCTUnwrap(alert.actions.first)
+                XCTAssertEqual(dismissAction.title, "Dismiss")
+                dismissAction.handler()
+            }
+            .verifyState { XCTAssert($0.updateWorker.isReady) }
+            .assertNoAction()
+            .assertNoOutput()
+    }
+    
+    func testUpdateWorkerSuccess() throws {
+        let workflow = DemoList.Workflow()
+        let expectation = expectation(description: "UpdateDemos")
+        
+        DemoList.Workflow.UpdateWorker
+            .working(to: workflow.updateDemos)
+            .run()
+            .startWithValues { result in
+                switch result {
+                case .success(Demo.allCases):
+                    expectation.fulfill()
+                default:
+                    break
+                }
+            }
+        
+        wait(for: [expectation])
+    }
+    
+    func testUpdateWorkerFailureLoadError() throws {
+        let workflow = DemoList.Workflow()
+        let expectation = expectation(description: "UpdateDemos")
+        
+        DemoList.Workflow.UpdateWorker
+            .working(to: workflow.updateDemos)
+            .run()
+            .startWithValues { result in
+                switch result {
+                case .failure(.loadError):
+                    expectation.fulfill()
+                default:
+                    break
+                }
+            }
+        
+        wait(for: [expectation])
+    }
 }
 
 // MARK: -
